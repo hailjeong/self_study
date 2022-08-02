bfa6117f83b89b6d11e505ea46c7f9d6966be508
hailjeong <hailjeong96@gamil.com>
2022-08-02 13:41:07 +0900

    22.08.02 Styling

--- styling-react/src/App.js
+++ styling-react/src/App.js
@@ -1,14 +1,11 @@
-import React, { Component } from "react";
-import SassComponent from "./SassComponent";
+import StyledComponent from "./StyledComponent";
 
-class App extends Component {
-  render() {
-    return (
-      <div>
-        <SassComponent />
-      </div>
-    );
-  }
-}
+const App = () => {
+  return (
+    <div>
+      <StyledComponent />
+    </div>
+  );
+};
 
 export default App;
---
+++ styling-react/src/CSSModule.js
@@ -0,0 +1,10 @@
+import styles from "./CSSModule.module.scss";
+
+const CSSModule = () => {
+  return (
+    <div className={`${styles.wrapper} ${styles.inverted}`}>
+      안녕하세요, 저는<sapn className="something">CSS Module</sapn>
+    </div>
+  );
+};
+export default CSSModule;
