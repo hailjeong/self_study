91b197f43f3e534eae6bfe825a31bb432d793db1
hailjeong <hailjeong96@gamil.com>
2022-08-17 07:04:11 +0900

    22.08.17 Context API

---
+++ context-tutorial/src/App.js
@@ -0,0 +1,16 @@
+import ColorBox from "./components/ColorBox";
+import SelectColors from "./components/SelectColors";
+import { ColorProvider } from "./contexts/color";
+
+const App = () => {
+  return (
+    <ColorProvider value={{ color: "red" }}>
+      <div>
+        <SelectColors />
+        <ColorBox />
+      </div>
+    </ColorProvider>
+  );
+};
+
+export default App;
